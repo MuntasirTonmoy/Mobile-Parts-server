05cbc1a3b5f6667f66dcfb9c7e8dd73c102b0c57
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-05-29 05:21:04 +0600

    parts load done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const res = require("express/lib/response");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -9,6 +10,35 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
+
+//Mongodb
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yfl4a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    console.log("db connected");
+    const partsCollection = client.db("mobileParts").collection("parts");
+
+    // getting all the parts
+    app.get("/parts", async (req, res) => {
+      const query = {};
+      const cursor = partsCollection.find(query);
+      const parts = await cursor.toArray();
+      res.send(parts);
+    });
+  } finally {
+  }
+};
+
+//calling run function
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Manufacturer Server is running ");
