17a41b0aa813ba51f8996839a478cf701d037d6d
Muntasir Tonmoy <muntasir01521566317@gmail.com>
2022-06-04 21:54:37 +0600

    cors problem

--- index.js
+++ index.js
@@ -10,8 +10,24 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors({ origin: "http://localhost:3000" }));
+/* app.use(cors());
+app.use(express.json()); */
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+};
+app.use(cors(corsConfig));
+app.options("*", cors(corsConfig));
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,authorization"
+  );
+  next();
+});
 
 const verifyToken = (req, res, next) => {
   const header = req.headers.authorization;
